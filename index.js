a393edab9d5875b1dc613b0847c9c55bbe051d0e
SebZG <sebastian.zapata.g@gmail.com>
2024-02-18 20:46:19 +0000

    added promptEngineer()

--- index.js
+++ index.js
@@ -38,3 +38,28 @@ const promptManager = () => {
     },
   ]);
 }
+
+const promptEngineer = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the engineer's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the engineer's id?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the engineer's email?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is the engineer's github?",
+    },
+  ]);
+}
