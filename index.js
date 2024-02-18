b73d82e5e2c896ffaded402b891875730ed91922
SebZG <sebastian.zapata.g@gmail.com>
2024-02-18 22:09:44 +0000

    clean up

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 // utils
+const {
+  promptManager,
+  promptAddTeamMembers,
+  promptEngineer,
+  promptInern } = require("./utils/prompts.js");
 const render = require("./src/page-template.js");
-const { promptManager, promptAddTeamMembers, promptEngineer, promptInern } = require("./utils/prompts.js");
 
 const OUTPUT_DIR = path.resolve(__dirname, "./output/");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
