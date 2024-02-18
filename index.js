978c6402b89c6a24ac37c95610b5afe863585769
SebZG <sebastian.zapata.g@gmail.com>
2024-02-18 21:19:53 +0000

    Moved prompts to seperate file/folder

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// modules
-const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
 // classes
@@ -8,97 +6,12 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 // utils
 const render = require("./src/page-template.js");
+const { promptManager, promptAddTeamMembers, promptEngineer, promptInern } = require("./utils/prompts.js");
 
 const OUTPUT_DIR = path.resolve(__dirname, "./output/");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const teamMembers = [];
-
-const promptManager = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is the team manager's name?",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is the team manager's id?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is the team manager's email?",
-    },
-    {
-      type: "input",
-      name: "officeNumber",
-      message: "What is the team manager's office number?",
-    },
-  ]);
-}
-
-const promptEngineer = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is the engineer's name?",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is the engineer's id?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is the engineer's email?",
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "What is the engineer's github?",
-    },
-  ]);
-}
-
-const promptInern = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is the intern's name?",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is the intern's id?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is the intern's email?",
-    },
-    {
-      type: "input",
-      name: "school",
-      message: "What is the intern's school?",
-    },
-  ]);
-}
-
-const promptAddTeamMembers = () => {
-  return inquirer.prompt([
-    {
-      type: "list",
-      name: "addTeamMembers",
-      message: "Select type of team member to add",
-      choices: ["Engineer", "Intern", "None"],
-    }
-  ]);
-}
 
 const init = async () => {
 
---
+++ utils/prompts.js
@@ -0,0 +1,88 @@
+const inquirer = require("inquirer");
+
+const promptManager = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the team manager's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the team manager's id?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the team manager's email?",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is the team manager's office number?",
+    },
+  ]);
+};
+exports.promptManager = promptManager;
+const promptEngineer = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the engineer's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the engineer's id?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the engineer's email?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is the engineer's github?",
+    },
+  ]);
+};
+exports.promptEngineer = promptEngineer;
+const promptInern = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the intern's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the intern's id?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the intern's email?",
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "What is the intern's school?",
+    },
+  ]);
+};
+exports.promptInern = promptInern;
+const promptAddTeamMembers = () => {
+  return inquirer.prompt([
+    {
+      type: "list",
+      name: "addTeamMembers",
+      message: "Select type of team member to add",
+      choices: ["Engineer", "Intern", "None"],
+    }
+  ]);
+};
+exports.promptAddTeamMembers = promptAddTeamMembers;
